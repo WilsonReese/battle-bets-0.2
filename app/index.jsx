a98a9a7814eed0467f519dad382d02702844bb91
WilsonReese <71947861+WilsonReese@users.noreply.github.com>
2024-06-19 15:48:20 +0000

    trying to fix home screen

--- app/index.jsx
+++ app/index.jsx
@@ -1,26 +1,10 @@
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
-import { StatusBar } from "expo-status-bar";
-import { Link, router } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Home() {
   return (
     <>
       <View style={s.container}>
-        <StatusBar style="dark" />
-        <Text>Pool 1 Name</Text>
-        <Link href="/pools/1">Go to Pool 1</Link>
-        <Text>Pool 2 Name</Text>
-        <TouchableOpacity
-          onPress={() =>
-            router.push({
-              pathname: "/pools/[id]",
-              params: { id: 2 },
-            })
-          }
-        >
-          <Text>Go to Pool 2</Text>
-        </TouchableOpacity>
-        <Text>Create a pool</Text>
+        <Text>Home screen</Text>
       </View>
     </>
   );
