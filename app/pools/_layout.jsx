f4e1411018b7bb605d71c4c2e8186107867e1985
WilsonReese <71947861+WilsonReese@users.noreply.github.com>
2024-06-19 16:46:54 +0000

    getting bottom tab to have the correct screens

--- app/pools/_layout.jsx
+++ app/pools/_layout.jsx
@@ -3,7 +3,7 @@ import { Stack } from "expo-router";
 export default function PoolsLayout() {
   return (
     <Stack>
-      {/* <Stack.Screen name="index" options={{ title: "index" }} /> */}
+      <Stack.Screen name="index" options={{ title: "index" }} />
       <Stack.Screen name="[id]/index" options={{ title: 'Pool Details' }}/>
       <Stack.Screen name="create" options={{ title: "Create" }} />
 			<Stack.Screen name="[id]/picks" options={{ title: "picks" }} />
