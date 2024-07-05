1558575d581cd2da63b8e2bfe59f6bc24fbcb98f
WilsonReese <71947861+WilsonReese@users.noreply.github.com>
2024-07-05 20:27:05 +0000

    formatted bet options

--- components/GameCard/BetOption.jsx
+++ components/GameCard/BetOption.jsx
@@ -1,24 +1,23 @@
 import { Pressable, StyleSheet, View } from "react-native";
 import { Txt } from "../general/Txt";
 
-export function BetOption({ title, payout, isSelected, isEnabled, onPress }) {
-  function checkIfSelected() {
-    return isSelected ? s.isSelected : [s.isNotSelected, s.shadow];
-  }
+export function BetOption({ title, payout, isSelected, onPress, isEnabled }) {
+  const shadowStyle = isEnabled || isSelected ? s.shadow : null;
 
   return (
     <Pressable
       style={({ pressed }) => [
         s.optionView,
-        checkIfSelected(),
+        isSelected ? s.isSelected : s.isNotSelected,
         pressed && isEnabled && { opacity: 0.5 },
         !isEnabled && !isSelected && s.disabledOption, // Apply disabled style if not selected
+        shadowStyle, // Conditionally apply shadow style
       ]}
       onPress={isEnabled || isSelected ? onPress : null} // Disable onPress if not enabled and not selected
     >
-      <Txt style={s.oddsText}>{title}</Txt>
-      <View style={s.payout}>
-        <Txt style={s.payoutText}>x{payout}</Txt>
+      <Txt style={[ isEnabled || isSelected ? s.oddsText : s.disabledOddsText]}>{title}</Txt>
+      <View style={[isEnabled || isSelected ? s.payout: s.disabledPayout]}>
+        <Txt style={[ isEnabled || isSelected ? s.payoutText : s.disabledPayoutText]}>x{payout}</Txt>
       </View>
     </Pressable>
   );
@@ -39,7 +38,12 @@ const s = StyleSheet.create({
   },
   isNotSelected: {
     marginBottom: 4,
-    // shadow
+  },
+  isSelected: {
+    backgroundColor: "#54D18C",
+    borderBottomRightRadius: 0,
+    borderBottomLeftRadius: 0,
+    paddingBottom: 4,
   },
   shadow: {
     shadowColor: "#000",
@@ -51,18 +55,17 @@ const s = StyleSheet.create({
     shadowRadius: 3.84,
     elevation: 5,
   },
-  isSelected: {
-    backgroundColor: "#54D18C",
-    borderBottomRightRadius: 0,
-    borderBottomLeftRadius: 0,
-    paddingBottom: 4,
-  },
   disabledOption: {
-    backgroundColor: "#ddd", // Style for disabled state
+    backgroundColor: "#B8C3CC", 
   },
   oddsText: {
     fontFamily: "Saira_600SemiBold",
     color: "#061826",
+    fontSize: 14,
+  },
+  disabledOddsText: {
+    fontFamily: "Saira_600SemiBold",
+    color: "#6E7880",
     fontSize: 14,
   },
   payout: {
@@ -71,9 +74,20 @@ const s = StyleSheet.create({
     borderBottomRightRadius: 8,
     padding: 8,
   },
+  disabledPayout: {
+    backgroundColor: "#B8C3CC",
+    borderTopRightRadius: 8,
+    borderBottomRightRadius: 8,
+    padding: 8,
+  },
   payoutText: {
     fontFamily: "Saira_300Light",
     color: "#061826",
-    fontSize: "14",
+    fontSize: 14,
+  },
+  disabledPayoutText: {
+    fontFamily: "Saira_300Light",
+    color: "#6E7880",
+    fontSize: 14,
   },
 });
