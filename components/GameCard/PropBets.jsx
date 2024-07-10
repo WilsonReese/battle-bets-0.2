0ea4e1b97280b518a14a19589571a31e2b08b999
WilsonReese <71947861+WilsonReese@users.noreply.github.com>
2024-07-10 19:53:22 +0000

    got prop bets selector to work for individual options, not selecting all of them

--- components/GameCard/PropBets.jsx
+++ components/GameCard/PropBets.jsx
@@ -6,49 +6,44 @@ import { useBetLogic } from "../../hooks/useBetLogic";
 import { BETTING_RULES } from "../../utils/betting-rules";
 
 export function PropBets({ betOptions }) {
-  const {
-    selection,
-    isEnabled,
-    animatedHeight,
-    toggleBet,
-    betType,
-    currentBetId,
-  } = useBetLogic("prop");
-  const { minBet, maxBet } = BETTING_RULES[betType];
-
-  const renderPropOptions = (betOptions) => {
-    return betOptions.map((option) => (
-      <View>
-        <BetOption
-          title={option.name}
-          payout={option.payout}
-          isSelected={selection.optionOne}
-          isEnabled={isEnabled}
-          onPress={() => toggleBet("optionOne")}
-        />
-        <Animated.View style={{ height: animatedHeight, overflow: "hidden" }}>
-          {selection.optionOne && (
-            <BetSelector
-              option="optionOne"
-              closeSelection={() => toggleBet("optionOne")}
-              minBet={minBet}
-              maxBet={maxBet}
+    const renderPropOptions = (betOptions) => {
+      return betOptions.map((option) => {
+        const { selection, isEnabled, animatedHeight, toggleBet, betType, currentBetId } = useBetLogic("prop", option.name, option.name, option.payout, option.id);
+        const { minBet, maxBet } = BETTING_RULES[betType];
+  
+        return (
+          <View key={option.name}>
+            <BetOption
+              title={option.name}
               payout={option.payout}
-              betId={currentBetId}
+              isSelected={selection.optionOne}
+              isEnabled={isEnabled}
+              onPress={() => toggleBet("optionOne")}
             />
-          )}
-        </Animated.View>
+            <Animated.View style={{ height: animatedHeight, overflow: "hidden" }}>
+              {selection.optionOne && (
+                <BetSelector
+                  option="optionOne"
+                  closeSelection={() => toggleBet("optionOne")}
+                  minBet={minBet}
+                  maxBet={maxBet}
+                  payout={option.payout}
+                  betId={currentBetId}
+                />
+              )}
+            </Animated.View>
+          </View>
+        );
+      });
+    };
+  
+    return (
+      <View>
+        <BetTypeHeading heading={"Prop Bets"} />
+        {renderPropOptions(betOptions)}
       </View>
-    ));
-  };
-
-  return (
-    <View>
-      <BetTypeHeading heading={"Prop Bets"} />
-      {renderPropOptions(betOptions)}
-    </View>
-  );
-}
+    );
+  }
 
 const s = StyleSheet.create({
   optionsContainer: {
