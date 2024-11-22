7daff3876181b77a53b06aed650aa6adf101cd1d
Reese Wilson <reese@chicagobooth.edu>
2024-11-22 12:29:05 -0600

    changed from dotenv to expo CLI

--- utils/api.js
+++ utils/api.js
@@ -1,5 +1,3 @@
-import { API } from "@env";
+export const API_BASE_URL = process.env.EXPO_PUBLIC_API_URL;
 
-console.log("Using API_BASE_URL:", API);
-
-export const API_BASE_URL = API;
+console.log("Using API_BASE_URL:", API_BASE_URL);
