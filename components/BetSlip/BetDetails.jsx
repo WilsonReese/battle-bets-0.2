bd41783bf75fbddd6464d968e155b82cd2d09de2
Reese Wilson <reese@chicagobooth.edu>
2024-12-05 20:20:33 -0600

    changed the bet details to be more balanced

--- components/BetSlip/BetDetails.jsx
+++ components/BetSlip/BetDetails.jsx
@@ -4,62 +4,41 @@ import { Txt } from "../general/Txt";
 
 export function BetDetails({ name, matchup, time, multiplier }) {
   return (
-    <View style={[s.container]}>
-      <View style={s.matchupContainer}>
-        <Txt style={[s.smallTxt]}>{matchup}</Txt>
-        {/* <Txt style={[s.smallTxt]}>{time}</Txt> */}
-      </View>
-      <View style={s.betInfoContainer}>
-        <View style={s.betNameContainer}>
-          <Txt style={[s.betNameTxt]}>{name}</Txt>
-        </View>
-        <View style={s.betPayoutContainer}>
-          <Txt style={[s.payoutTxt]}>x{multiplier}</Txt>
-        </View>
-      </View>
-    </View>
-		// <View style={s.container}>
-		// 	<View style={s.betDetailsContainer}>
-		// 		<View>
-		// 			<Txt style={[s.smallTxt]}>{matchup}</Txt>
-		// 		</View>
-		// 		<View>
-		// 			<Txt style={[s.betNameTxt]}>{name}</Txt>
-		// 		</View>
-		// 	</View>
-		// 	<View>
-		// 		<Txt style={[s.payoutTxt]}>x{multiplier}</Txt>
-		// 	</View>
-		// </View>
+		<View style={s.container}>
+			<View style={s.betDetailsContainer}>
+				<View>
+					<Txt style={[s.smallTxt]}>{matchup}</Txt>
+				</View>
+				<View>
+					<Txt style={[s.betNameTxt]}>{name}</Txt>
+				</View>
+			</View>
+			<View style={s.betPayoutContainer}>
+				<Txt style={[s.payoutTxt]}>x{multiplier}</Txt>
+			</View>
+		</View>
   );
 }
 
 const s = StyleSheet.create({
-  container: {
-    paddingVertical: 4,
-  },
-  betInfoContainer: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    // paddingVertical: 2,
-  },
-  betNameContainer: {
-    flex: 4,
+	container: {
+		paddingVertical: 4,
 		flexDirection: 'row',
-		paddingRight: 8,
-    // justifyContent: 'flex-end',
-    // alignItems: 'center',
-    // backgroundColor: 'green'
-  },
-  betPayoutContainer: {
-    flex: 1,
+		alignItems: 'center',
+	},
+	betDetailsContainer: {
+		flex: 1,
+	},
+	betPayoutContainer: {
+		// flex: 1,
     alignItems: "center",
     justifyContent: "center",
     // paddingLeft: 4,
 		borderRadius: 4,
 		backgroundColor: '#54D18C',
-  },
+		height: 20,
+		width: 40,
+	},
   betNameTxt: {
     fontFamily: "Saira_600SemiBold",
     fontSize: 15,
@@ -67,10 +46,6 @@ const s = StyleSheet.create({
     textAlignVertical: "center",
     paddingTop: 3,
 		color: "#061826",
-  },
-  matchupContainer: {
-    flexDirection: "row",
-    // justifyContent: "space-between",
   },
 	payoutTxt: {
 		color: "#061826",
