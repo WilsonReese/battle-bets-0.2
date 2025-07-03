90487d2091ef63e6a5485f0367e6a54c3f1d5756
Reese Wilson <reese@battlebets.app>
2025-07-03 10:52:33 -0500

    changed invite users button linking

--- components/PoolDetails/InviteUsers/InviteUsersButton.jsx
+++ components/PoolDetails/InviteUsers/InviteUsersButton.jsx
@@ -1,37 +1,49 @@
+import * as Linking from "expo-linking";
 import { StyleSheet, TouchableOpacity } from "react-native";
 import { Txt } from "../../general/Txt";
-import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
-import * as Clipboard from 'expo-clipboard';
+import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
+import * as Clipboard from "expo-clipboard";
 import { useToastMessage } from "../../../hooks/useToastMessage";
 
-export function InviteUsersButton({ poolId, inviteToken, style, textStyle, iconColor = "#54D18C", }) {
-  const { showSuccess, showError } = useToastMessage();
+export function InviteUsersButton({
+	poolId,
+	inviteToken,
+	style,
+	textStyle,
+	iconColor = "#54D18C",
+}) {
+	const { showSuccess, showError } = useToastMessage();
 
-  const handleCopyLink = async () => {
-    const link = `exp://10.139.1.76:8081/--/join?pool_id=${poolId}&token=${inviteToken}`;
-    await Clipboard.setStringAsync(link);
-    showSuccess("Invitation link copied!");
-  };
+	const handleCopyLink = async () => {
+		const url = Linking.createURL("/join", {
+			queryParams: {
+				pool_id: poolId,
+				token: inviteToken,
+			},
+		});
 
-  return (
-    <>
-      <TouchableOpacity style={[s.button, style]} onPress={handleCopyLink}>
-        <Txt style={textStyle}>Copy League Invitation Link</Txt>
-        <FontAwesome6 name="link" size={16} color={iconColor} />
-      </TouchableOpacity>
-    </>
-  );
+		await Clipboard.setStringAsync(url);
+		showSuccess("Invitation link copied!");
+	};
+
+	return (
+		<>
+			<TouchableOpacity style={[s.button, style]} onPress={handleCopyLink}>
+				<Txt style={textStyle}>Copy League Invitation Link</Txt>
+				<FontAwesome6 name="link" size={16} color={iconColor} />
+			</TouchableOpacity>
+		</>
+	);
 }
 
 const s = StyleSheet.create({
-  button: {
-    // paddingHorizontal: 12,
-    paddingVertical: 8,
-    flexDirection: "row",
-    justifyContent: 'center',
-    alignItems: "center",
-    gap: 8,
-    // backgroundColor: 'green'
-  },
-
+	button: {
+		// paddingHorizontal: 12,
+		paddingVertical: 8,
+		flexDirection: "row",
+		justifyContent: "center",
+		alignItems: "center",
+		gap: 8,
+		// backgroundColor: 'green'
+	},
 });
