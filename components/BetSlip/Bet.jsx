e446d3f27c781e5f0c19f25fb0dfbabbc34e67d6
Reese Wilson <reese@battlebets.app>
2025-07-09 17:38:38 -0500

    got bet selector to open on the betslip

--- components/BetSlip/Bet.jsx
+++ components/BetSlip/Bet.jsx
@@ -133,95 +133,99 @@
 // 	},
 // });
 
-import React from "react";
-import {
-  StyleSheet,
-  View,
-  TouchableOpacity,
-} from "react-native";
+import React, { useEffect, useRef, useState } from "react";
+import { StyleSheet, View, TouchableOpacity, Animated } from "react-native";
 import { Txt } from "../general/Txt";
 import { BetDetails } from "./BetDetails";
 import { BetAmount } from "./BetAmount";
 import { format } from "date-fns";
 import { useBetStore } from "../../state/useBetStore";
+import { BETTING_RULES } from "../../utils/betting-rules";
+import { BetSelector } from "../GameCard/BetSelector";
 
 export const Bet = React.memo(
-  function Bet({ betOptionId }) {
-    // subscribe to only this bet’s slice
+	function Bet({ betOptionId }) {
+		// subscribe to only this bet’s slice
+		const bet = useBetStore((state) =>
+			state.bets.find((b) => b.bet_option_id === betOptionId)
+		);
+		if (!bet) return null;
 
-    console.log('Bet Option ID', betOptionId)
+		const [isOpen, setIsOpen] = useState(false);
+		const animatedHeight = useRef(new Animated.Value(0)).current;
+		const removeBet = useBetStore((state) => state.removeBet);
 
-    const bet = useBetStore(
-      (state) => state.bets.find((b) => b.bet_option_id === betOptionId)
-    );
+		const matchup = `${bet.game.away_team.name} at ${bet.game.home_team.name}`;
+		const gameTime = format(new Date(bet.game.start_time), "h:mm a");
 
-    console.log('Bet', bet)
+		const { minBet, maxBet } = BETTING_RULES[bet.category];
 
-    if (!bet) return null;
+		useEffect(() => {
+			Animated.timing(animatedHeight, {
+				toValue: isOpen ? 54 : 0,
+				duration: 100,
+				useNativeDriver: false,
+			}).start();
+		}, [isOpen]);
 
-    const matchup = `${bet.game.away_team.name} at ${bet.game.home_team.name}`;
-    const gameTime = format(new Date(bet.game.start_time), "h:mm a");
-
-    return (
- 		<View style={[s.container]}>
-			<TouchableOpacity
-				style={[
-					s.betItem,
-					// isBetSelectorOpen && s.betItemEditMode,
-				]}
-				// onPress={() => toggleBetSelector(bet.id)}
-			>
-				<View style={s.betDetailsContainer}>
-					<View style={s.betNameContainer}>
-						{/* <Txt style={s.betNameText}>{bet.name}</Txt> */}
-						<BetDetails
-							name={bet.title}
-							multiplier={bet.payout}
-							matchup={matchup}
-							time={gameTime}
-							betNameColor={"#F8F8F8"}
-						/>
+		return (
+			<View style={[s.container]}>
+				<TouchableOpacity
+					style={[s.betItem, isOpen && s.betItemEditMode]}
+					onPress={() => setIsOpen(!isOpen)}
+				>
+					<View style={s.betDetailsContainer}>
+						<View style={s.betNameContainer}>
+							{/* <Txt style={s.betNameText}>{bet.name}</Txt> */}
+							<BetDetails
+								name={bet.title}
+								multiplier={bet.payout}
+								matchup={matchup}
+								time={gameTime}
+								betNameColor={"#F8F8F8"}
+							/>
+						</View>
+						<View style={s.betAmountContainer}>
+							<BetAmount
+								bet_amount={bet.bet_amount}
+								to_win_amount={bet.to_win_amount}
+							/>
+						</View>
 					</View>
-					<View style={s.betAmountContainer}>
-						<BetAmount
-							bet_amount={bet.bet_amount}
-							to_win_amount={bet.to_win_amount}
-						/>
-					</View>
-				</View>
-			</TouchableOpacity>
-			{/* <Animated.View
-        style={[
-          s.betSelectorContainer,
-          {
-            height: animatedHeight,
-            overflow: "hidden",
-            opacity: animatedHeight.interpolate({
-              inputRange: [0, 54],
-              outputRange: [0, 1],
-            }),
-          },
-        ]}
-      >
-        <BetSelector
-          betId={bet.id}
-          closeSelection={() => {
-            removeBet(bet.id);
-          }}
-          minBet={minBet}
-          maxBet={maxBet}
-          payout={bet.toWinAmount / bet.betAmount}
-        />
-      </Animated.View> */}
-		</View>
-    );
-  },
-  // only re-render if the *props* change
-  (prev, next) =>
-    prev.betOptionId === next.betOptionId &&
-    prev.backgroundColor === next.backgroundColor
+				</TouchableOpacity>
+
+				<Animated.View
+					style={[
+						s.betSelectorContainer,
+						{
+							height: animatedHeight,
+							opacity: animatedHeight.interpolate({
+								inputRange: [0, 54],
+								outputRange: [0, 1],
+							}),
+							overflow: "hidden",
+						},
+					]}
+				>
+					<BetSelector
+						bet={bet}
+						closeSelection={() => {
+							removeBet(bet.bet_option_id);
+							setIsOpen(false);
+						}}
+						minBet={minBet}
+						maxBet={maxBet}
+						payout={bet.payout}
+					/>
+				</Animated.View>
+			</View>
+		);
+	},
+	// only re-render if the *props* change
+	(prev, next) =>
+		prev.betOptionId === next.betOptionId &&
+		prev.backgroundColor === next.backgroundColor
 );
-
 
 const s = StyleSheet.create({
 	container: {
@@ -235,7 +239,7 @@ const s = StyleSheet.create({
 		paddingHorizontal: 8,
 		paddingVertical: 4,
 		// borderWidth: .5,
-    backgroundColor: '#0F2638',
+		backgroundColor: "#0F2638",
 		borderRadius: 8,
 	},
 	betItemEditMode: {
